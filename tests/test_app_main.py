ee6a6f4b6dc36865fab214bfaf80d0f4b1fc1ad7
asger1537 <asger1537@gmail.com>
2024-01-16 14:10:58 +0100

    Added skips to app tests

--- tests/test_app_main.py
+++ tests/test_app_main.py
@@ -1,13 +1,19 @@
 import pytest
-from API.app.main import app
 from fastapi.testclient import TestClient
 from io import BytesIO
 from PIL import Image
+import os
 
+models_dir_exists = os.path.isdir("models")
+if models_dir_exists:
+    from API.app.main import app
+
+@pytest.mark.skipif(not models_dir_exists, reason="Models directory does not exist")
 @pytest.fixture
 def client():
     return TestClient(app)
 
+@pytest.mark.skipif(not models_dir_exists, reason="Models directory does not exist")
 @pytest.fixture
 def test_image():
     # Create a simple image for testing
@@ -17,10 +23,12 @@ def test_image():
     img_byte_arr.seek(0)  # Important: reset buffer to the beginning
     return img_byte_arr
 
+@pytest.mark.skipif(not models_dir_exists, reason="Models directory does not exist")
 def test_connection(client):
     response = client.get("/")
     assert response.status_code == 200
 
+@pytest.mark.skipif(not models_dir_exists, reason="Models directory does not exist")
 def test_inference_with_vit(client, test_image):
     files = {
         "data": ("test_image.jpg", test_image, "image/jpeg"),
@@ -33,6 +41,7 @@ def test_inference_with_vit(client, test_image):
     assert "image" in response_json
     assert "model_name" in response_json
 
+@pytest.mark.skipif(not models_dir_exists, reason="Models directory does not exist")
 def test_inference_with_simplecnn(client, test_image):
     files = {
         "data": ("test_image.jpg", test_image, "image/jpeg"),
