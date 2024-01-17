08502ab08a06bbf6fc9ca9e41b667ae0b06ce947
Kasperjoergensen3 <Kasperjoergensen3@gmail.com>
2024-01-17 10:33:35 +0100

    api_with_drift

--- API/app/main.py
+++ API/app/main.py
@@ -14,78 +14,101 @@ from PIL import Image
 from time import time
 from datetime import datetime
 from evidently.report import Report
-from evidently.metric_preset import DataDriftPreset, DataQualityPreset, TargetDriftPreset
+from evidently.metric_preset import (
+    DataDriftPreset,
+    DataQualityPreset,
+    TargetDriftPreset,
+)
 from prometheus_fastapi_instrumentator import Instrumentator
 from google.cloud import storage
 
 from src.data.make_dataset import transform_image
 from src.utilities.modules import recursive_find_python_class
 
+
 class ItemEnum(Enum):
     ViT = "ViT"
     SimpleCNN = "SimpleCNN"
 
+
 def load_models():
     models = {}
+    BUCKET_NAME = "models_group29"
+    client = storage.Client(project="mlopsproject")
+    bucket = client.get_bucket(BUCKET_NAME)
     for model_name in ItemEnum:
         model_path = Path("models/test_predict/{}".format(model_name.value))
-        config_path = model_path.joinpath(".hydra", "config.yaml")
+        config_path = model_path.joinpath("hydra", "config.yaml")
         checkpoint_path = model_path.joinpath("checkpoints", "best-checkpoint.ckpt")
+        blob = bucket.blob(str(config_path))
+        config_path.parent.mkdir(parents=True, exist_ok=True)
+        blob.download_to_filename(str(config_path))
         config = OmegaConf.load(config_path)
         module = recursive_find_python_class(config.model.name)
         model = module(config)
+        blob = bucket.blob(str(checkpoint_path))
+        checkpoint_path.parent.mkdir(parents=True, exist_ok=True)
+        blob.download_to_filename(str(checkpoint_path))
         state_dict = torch.load(checkpoint_path)["state_dict"]
         model.load_state_dict(state_dict)
         model.eval()
         models[model_name.value] = model
     return models
 
-def add_feature_vec_to_DB(now:str, img:torch.Tensor, class_pred:str):
+
+def add_feature_vec_to_DB(now: str, img: torch.Tensor, class_pred: str):
     mean = torch.mean(img)
     contrast = torch.std(img)
     # Initialize the client
     BUCKET_NAME = "api_user_inputs"
     MODEL_FILE = "input.csv"
-    client = storage.Client(project='mlopsproject')
+    client = storage.Client(project="mlopsproject")
     bucket = client.get_bucket(BUCKET_NAME)
 
     # Download the file
     blob = bucket.blob(MODEL_FILE)
     blob.download_to_filename(MODEL_FILE)
 
-    #Add feature to database
+    # Add feature to database
     with open(MODEL_FILE, "a") as file:
         file.write(f"\n{now}, {mean}, {contrast}, {class_pred}")
 
-    #Upload file to GCP
+    # Upload file to GCP
     blob.upload_from_filename(MODEL_FILE)
-    return 
+    return
 
-client = storage.Client(project='mlopsproject')
+
+client = storage.Client(project="mlopsproject")
 data_bucket = client.get_bucket("data_splits_group29")
 blob_data = data_bucket.blob("data/processed/train_features.csv")
+# data/processed/ make dir if not exists
+Path("data/processed").mkdir(parents=True, exist_ok=True)
 blob_data.download_to_filename("data/processed/train_features.csv")
 train_data = pd.read_csv("data/processed/train_features.csv")
 
-def data_drift_func(): 
-    client = storage.Client(project='mlopsproject')
+
+def data_drift_func():
+    client = storage.Client(project="mlopsproject")
     bucket = client.get_bucket("api_user_inputs")
 
     # Download the file
     blob = bucket.blob("input.csv")
     blob.download_to_filename("API/app/input.csv")
     inf_data = pd.read_csv("API/app/input.csv")
-    inf_data.drop(["time"], axis = 1, inplace=True)
+    inf_data.drop(["time"], axis=1, inplace=True)
 
-    #Generate report
-    report = Report(metrics=[DataDriftPreset(), DataQualityPreset(), TargetDriftPreset()])
+    # Generate report
+    report = Report(
+        metrics=[DataDriftPreset(), DataQualityPreset(), TargetDriftPreset()]
+    )
     report.run(reference_data=train_data, current_data=inf_data)
-    report.save_html('API/app/report.html')
+    report.save_html("API/app/report.html")
 
-    #Upload report to GCP
+    # Upload report to GCP
     blob = bucket.blob("report.html")
     blob.upload_from_filename("API/app/report.html")
     return
+
 
 app = FastAPI()
 counter = 0
@@ -93,26 +116,29 @@ start = time()
 models = load_models()
 print("Model loaded in {} seconds".format(time() - start))
 
+
 @app.get("/", response_class=HTMLResponse)
 async def read_index():
     with open("API/app/app.html", "r") as f:
         return f.read()
-    
+
+
 @app.get("/drift-report", response_class=HTMLResponse)
 async def read_drift_report():
-    client = storage.Client(project='mlopsproject')
+    client = storage.Client(project="mlopsproject")
     bucket = client.get_bucket("api_user_inputs")
 
     # Download the file
     blob = bucket.blob("report.html")
     blob.download_to_filename("API/app/drift_report.html")
-    with open("API/app/drift_report.html", "r", encoding='utf-8') as f:
+    with open("API/app/drift_report.html", "r", encoding="utf-8") as f:
         html_content = f.read()
         return HTMLResponse(content=html_content, status_code=200)
 
+
 @app.post("/inference/")
 async def inference(request: Request, background_tasks: BackgroundTasks):
-    global counter 
+    global counter
     form_data = await request.form()
     data = form_data["data"]
     model_name = form_data["model"]
@@ -120,7 +146,7 @@ async def inference(request: Request, background_tasks: BackgroundTasks):
         raise HTTPException(status_code=400, detail="File is not an image.")
     if model_name not in models:
         raise HTTPException(status_code=500, detail="Model not loaded.")
-    
+
     model = models[model_name]
 
     start = time()
@@ -140,10 +166,12 @@ async def inference(request: Request, background_tasks: BackgroundTasks):
         prediction = classes[top_class.item()]
 
     print("Prediction made in {} seconds".format(time() - start))
-    
+
     counter += 1
-    now = str(datetime.now())   
-    background_tasks.add_task(add_feature_vec_to_DB, now, image_tensor, int(top_class.item()))
+    now = str(datetime.now())
+    background_tasks.add_task(
+        add_feature_vec_to_DB, now, image_tensor, int(top_class.item())
+    )
 
     if counter % 4 == 0:
         background_tasks.add_task(data_drift_func)
@@ -153,7 +181,13 @@ async def inference(request: Request, background_tasks: BackgroundTasks):
     image.save(buffered, format="JPEG")
     img_base64 = base64.b64encode(buffered.getvalue()).decode()
 
-    return JSONResponse(content={"prediction": prediction, "image": img_base64, "model_name": model_name})
+    return JSONResponse(
+        content={
+            "prediction": prediction,
+            "image": img_base64,
+            "model_name": model_name,
+        }
+    )
 
 
 Instrumentator().instrument(app).expose(app)
--- dockerfiles/api.dockerfile
+++ dockerfiles/api.dockerfile
@@ -1,7 +1,7 @@
 # docker build -f dockerfiles/api.dockerfile . -t api:latest   
 # docker run -d --name mycontainer -p 80:80 api:latest  
-#docker tag api gcr.io/mlops-group29/api
-#docker push gcr.io/mlops-group29/api
+#docker tag api gcr.io/grounded-camp-410709/api
+#docker push gcr.io/grounded-camp-410709/api
 
 # with out datadrift and buckets
 # FROM python:3.10-slim
@@ -17,12 +17,16 @@
 # WORKDIR code/
 # CMD ["uvicorn", "API.app.main:app", "--host", "0.0.0.0", "--port", "80"]
 
+FROM python:3.10-slim
+
 COPY ./API/ code/API/
 COPY ./src/ code/src/
 COPY pyproject.toml code/pyproject.toml
 RUN pip install --no-cache-dir --upgrade -r code/API/app/requirements.txt
 RUN pip install -e code/
-RUN pip install python-multipart
+COPY requirements.txt code/requirements.txt
+RUN pip install -r code/requirements.txt
+
 
 WORKDIR code/
 CMD ["uvicorn", "API.app.main:app", "--host", "0.0.0.0", "--port", "80"]
